af214d9e9cd81eb89fd0c289040ed4bb0c91b388
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2020-08-01 13:12:07 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HBJquHycNtTLkcffNUDabhM5VgFMfUwBRZZ8xlFGsXekNDyBdJ7xiaedVqelBLHkSKJJjaZTirxeF99ysR1tdvl00riNVLO5K',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
